a9401b166a9b2e094fbc952cadc4fda21b2dbe25
shivmishra <shivankmishra01@gmail.com>
2025-07-25 21:10:48 -0400

    added vercel analytics trackings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@vercel/analytics/integrations/astro';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [vercel()],
   vite: {
     plugins: [tailwindcss()]
   }
